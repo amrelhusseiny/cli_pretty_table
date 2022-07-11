6ae3b4ef5f010b1335b51ea89811cffa58aaf827
Amr ElHusseiny <amroashram@hotmail.com>
2022-07-11 15:37:41 +0200

    Update cli_pretty_table.py

--- cli_pretty_table.py
+++ cli_pretty_table.py
@@ -1,15 +1,42 @@
 # cli_pretty_table.py
 
-# Standard Library 
 # Non-standard library 
-import texttable
+from texttable import Texttable
+# Standard Library 
+import argparse
+import json
+import sys
 
 def cli_pretty_table(list_of_dictionaries):
+    table = Texttable(max_width=100)
 
-	return
+    # Adding Column names to table
+    column_names = []
+    for item in list_of_dictionaries:
+        for key_name in item.keys():
+            if key_name not in column_names:
+                column_names.append(key_name)
+    table.add_row(column_names)
+    # Adding data to the table : 
+    for item in list_of_dictionaries:
+        row_to_add = []
+        for cell in column_names : 
+            # Usig exception handling to avoid error if column does not exist in a row 
+            try:
+                row_to_add.append(item[cell])
+            except:
+                row_to_add.append('')
+        table.add_row(row_to_add)
+    print(table.draw())
+    return
 
 def main():
-	# Main for testing the scipt :     
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--json", help="input a list of dictionaries")
+    args = parser.parse_args()
+    print(json.loads(args.json))
+    # Main for testing the scipt :     
+    # cli_pretty_table(args.json)
 
 if __name__ == "__main__":
     main()
