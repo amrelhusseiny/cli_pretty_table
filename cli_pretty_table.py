2c94185e91ea47a6853767972e797c877c11fbfe
Amr ElHusseiny <amroashram@hotmail.com>
2022-07-11 13:51:54 +0200

    Create cli_pretty_table.py

---
+++ cli_pretty_table.py
@@ -0,0 +1,15 @@
+# cli_pretty_table.py
+
+# Standard Library 
+# Non-standard library 
+import texttable
+
+def cli_pretty_table(list_of_dictionaries):
+
+	return
+
+def main():
+	# Main for testing the scipt :     
+
+if __name__ == "__main__":
+    main()
